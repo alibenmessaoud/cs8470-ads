5188f076284096c8a264fc951d2e81fc18d0f301
terrance.medina <terrance.medina@gmail.com>
2012-09-11 17:34:36 -0400

    Subclassed Transaction into ExplicitTransaction and RandomTransaction

--- src/ads/Transaction.scala
+++ src/ads/Transaction.scala
@@ -14,35 +14,31 @@ import Op._
  * @author Terrance Medina
  * @param tid The transaction identifier.
  * @param m The transaction manager object.
+ * @param body The set of statements to be executed by this transaction.
  */
 class Transaction (var tid: Int, m: TransactionManager) extends Actor {
 
-  private var randOps = false
-  private var randNum = 0
+  //private var randOps = false
+  protected var randNum = 0
 
+/*
   def this (tid: Int, m: TransactionManager, rand: Int) {
-    this(tid, m)
+    this(tid, m, null)
     randOps = true
     randNum = rand
+		
   } // this
+*/
 
-  def act () {
-      
-    if (randOps) {
-      // TODO implement
-    } else body()
+	def body() {}
+  def act () { } // act
 
-  } // act
 
-  /**
-   * The body of the transaction, if defined.
-   */
-  def body () { }  
 
   /**
    * Begin the transaction.
    */
-  private def begin () = m ! BeginMessage(this)
+  def begin () = m ! BeginMessage(this)
 
   /**
    * Read a value into the transaction from the database.
@@ -50,7 +46,7 @@ class Transaction (var tid: Int, m: TransactionManager) extends Actor {
    * @param oid The object identifier.
    * @return the value of the object.
    */
-  private def read (oid: Int) = m !? ReadMessage(this, oid)
+  def read (oid: Int) = m !? ReadMessage(this, oid)
     
   /**
    * Write a value into the database.
@@ -58,12 +54,12 @@ class Transaction (var tid: Int, m: TransactionManager) extends Actor {
    * @param oid The object identifier.
    * @param value The value to write into the database.
    */
-  private def write (oid: Int, value: Any) = m ! WriteMessage(this, oid, value)
+  def write (oid: Int, value: Any) = m ! WriteMessage(this, oid, value)
 
   /**
    * Commit this transaction.
    */
-  private def commit () = {
+  def commit () = {
     m ! CommitMessage(this)
     exit()
   } // commit
@@ -83,4 +79,18 @@ class Transaction (var tid: Int, m: TransactionManager) extends Actor {
 
 } // Transaction class
 
-
+class ExplicitTransaction (var _tid: Int, _m: TransactionManager, explicit_body: ()=>Unit) extends Transaction(_tid, _m) {
+	override def act()
+	{
+		explicit_body()
+	}
+}
+class RandomTransaction (var _tid: Int, _m: TransactionManager, rand: Int) extends Transaction(_tid, _m) {
+	
+	override def act()
+	{
+		var i = 0
+		while(i<rand){println("Hello!");i=i+1;}
+		
+	}
+}
