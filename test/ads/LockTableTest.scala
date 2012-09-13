e8a05629b311b27af609e4a90dfec78e75bdb4c8
terrance.medina <terrance.medina@gmail.com>
2012-09-12 22:52:38 -0400

    Added test stub for Lock Table

---
+++ test/ads/LockTableTest.scala
@@ -0,0 +1,14 @@
+package ads.test
+
+import org.scalatest.FunSuite
+import ads.TransactionManager
+import ads.util.LockTable
+
+
+class LockTableTest extends FunSuite
+{
+  test("Lock Table")
+  {
+
+  }
+}
