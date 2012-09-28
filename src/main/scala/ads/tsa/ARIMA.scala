45c6faed932220cdfdc5e01314f0ccb134034134
Michael Cotterell <mepcotterell@gmail.com>
2012-09-28 10:31:58 -0400

    started work on ARIMA

---
+++ src/main/scala/ads/tsa/ARIMA.scala
@@ -0,0 +1,14 @@
+package ads.tsa
+
+import scalation.math.Vectors._
+
+object Autoregressive {
+
+  val x = new VectorD(1, 2, 3)
+
+} // Autoregressive
+
+object MovingAverage {
+
+} // MovingAverage
+
